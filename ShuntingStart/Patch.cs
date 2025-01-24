2b2e6aa26a5adca153dab323e619d4f89702f224
MikevanBreePXL <mike.vanbree@student.pxl.be>
2025-01-24 01:27:53 +0100

    Add Transpiler for StartGameData_NewCareer

--- ShuntingStart/Patch.cs
+++ ShuntingStart/Patch.cs
@@ -4,14 +4,14 @@ using HarmonyLib;
 
 namespace ShuntingStart
 {
-    [HarmonyPatch(typeof(LicenseManager))]
-    [HarmonyPatch("LoadData")]
-    public static class LicenseManagerLoadDataPatch
-    {
+	[HarmonyPatch(typeof(LicenseManager))]
+	[HarmonyPatch("LoadData")]
+	public static class LicenseManagerLoadDataPatch
+	{
         // Change original call from FreightHaul to Shunting
         [HarmonyTranspiler]
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
+		{
             /*
             // Replace IL-code
             //   IL_0112: ldc.i4    512
@@ -33,7 +33,39 @@ namespace ShuntingStart
             }
 
             return instructions;
-        }
+		}
+	}
 
+
+	[HarmonyPatch(typeof(StartGameData_NewCareer))]
+    [HarmonyPatch("Initialize")]
+    public static class StartGameDataNewCareerPatch
+    {
+        // Change original call from FreightHaul to Shunting
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            /*
+            // Replace IL-code
+            //    IL_0211: ldc.i4    512
+            // with
+            //   IL_0112: ldc.i4    1024
+
+                JobLicenseType_v1 are enum values
+		    */
+
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4 && (int)codes[i].operand == 512)
+                {
+                    codes[i].operand = 1024;
+                    break;
+                }
+            }
+
+            return instructions;
+        }
     }
 }
