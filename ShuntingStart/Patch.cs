7bd3143d4650fad6859d1ef11e54ca19770cdafb
MikevanBreePXL <mike.vanbree@student.pxl.be>
2025-01-24 03:29:38 +0100

    find a breakthrough or two and learn how to use the IL-code transpiler

--- ShuntingStart/Patch.cs
+++ ShuntingStart/Patch.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Reflection.Emit;
+using DV.Booklets;
+using DV.ThingTypes;
+using DV.ThingTypes.TransitionHelpers;
+using DV.Utils;
 using HarmonyLib;
+using UnityEngine;
 
 namespace ShuntingStart
 {
-	[HarmonyPatch(typeof(LicenseManager))]
-	[HarmonyPatch("LoadData")]
-	public static class LicenseManagerLoadDataPatch
-	{
+    [HarmonyPatch(typeof(LicenseManager), "LoadData")]
+    public static class LicenseManagerLoadDataPatch
+    {
         // Change original call from FreightHaul to Shunting
         [HarmonyTranspiler]
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-		{
+        {
             /*
             // Replace IL-code
             //   IL_0112: ldc.i4    512
@@ -22,26 +26,36 @@ namespace ShuntingStart
 			*/
 
             var codes = new List<CodeInstruction>(instructions);
+            int replacements = 0;
 
             for (var i = 0; i < codes.Count; i++)
             {
                 if (codes[i].opcode == OpCodes.Ldc_I4 && (int)codes[i].operand == 512)
                 {
                     codes[i].operand = 1024;
-                    break;
+                    replacements++;
+                    Debug.Log($"ShuntingStart: Replaced FreightHaul(512) with Shunting(1024) at index {i}");
                 }
             }
 
-            return instructions;
-		}
-	}
+            Debug.Log($"ShuntingStart: Made {replacements} replacements in LicenseManagerLoadDataPatch");
+            return codes;
+        }
+
+        [HarmonyPostfix]
+        static void Postfix(LicenseManager __instance)
+        {
+            JobLicenses.FreightHaul.ToV2().price = 10000f; // 10k
+        }
+    }
 
 
-	[HarmonyPatch(typeof(StartGameData_NewCareer))]
-    [HarmonyPatch("Initialize")]
+    [HarmonyPatch(typeof(StartGameData_NewCareer))]
     public static class StartGameDataNewCareerPatch
     {
         // Change original call from FreightHaul to Shunting
+        //[HarmonyPatch("PrepareNewSaveData")] // Build 99
+        [HarmonyPatch("Initialize")] // Build 98
         [HarmonyTranspiler]
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
@@ -52,20 +66,23 @@ namespace ShuntingStart
             //   IL_0112: ldc.i4    1024
 
                 JobLicenseType_v1 are enum values
-		    */
+            */
 
             var codes = new List<CodeInstruction>(instructions);
+            int replacements = 0;
 
             for (var i = 0; i < codes.Count; i++)
             {
                 if (codes[i].opcode == OpCodes.Ldc_I4 && (int)codes[i].operand == 512)
                 {
                     codes[i].operand = 1024;
-                    break;
+                    replacements++;
+                    Debug.Log($"ShuntingStart: Replaced FreightHaul(512) with Shunting(1024) at index {i}");
                 }
             }
 
-            return instructions;
+            return codes;
+            Debug.Log($"ShuntingStart: Made {replacements} replacements in StartGameDataNewCareerPatch");
         }
     }
 }
